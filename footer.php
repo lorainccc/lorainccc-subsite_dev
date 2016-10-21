54cfd7ca49c7db174a2db904b6a80593623ceea2
Joe Querin <joecue@gmail.com>
2016-10-21 13:33:53 -0400

    Revert "Added 404 Template and Updated Footer"
    This reverts commit de23311a81d5fbf826b22a838f122ffaa3b50a84.

--- footer.php
+++ footer.php
@@ -11,7 +11,23 @@
 
 	<footer id="colophon" class="small-12 medium-12 large-12 columns site-footer" role="contentinfo">
 		  <div class="row text-center medium-text-left">
--
+    <div class="large-3 medium-3 columns"> <img class="footer-logo" src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/lccclogo_white.svg" alt="" width="220" height="42.5"/>
+      <h2>Connect with LCCC</h2>
+      <ul class="menu footer-sm-links">
+        <li><a href="http://facebook.com/lorainccc" target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/facebook_white.svg" height="30" width="30" alt="" /></a></li>
+        <li><a href="http://twitter.com/lorainccc" target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/twitter_white.svg" height="30" width="30" alt="" /></a></li>
+        <li><a href="http://www.linkedin.com/company/lorain-county-community-college" target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/linkedin_white.svg" height="30" width="30" alt="" /></a></li>
+        <!--<li><a href="#" target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/pinterest_white.svg" height="30" width="30" alt="" /></a></li>-->
+        <li><a href="http://www.instagram.com/lorainccc" target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/instagram_white.svg" height="30" width="30" alt="" /></a></li>
+      </ul>
+      <a href="/campus-technology/lccc-mobile/" target="_blank" class="clearfix mobile-app-link"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/smartphone_yellow.svg" heigth="33" width="20" alt=""/>
+      <h2>LCCC'S<br />
+        Mobile App</h2>
+      </a>
+     <p class="website-feedback">
+      <a href="/website-feedback?siteurl=<?php echo 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; ?>" target="_blank">Feedback about this page</a>
+     </p>
+     </div>
     <div class="large-3 medium-3 columns">
       <h2>Contact LCCC</h2>
       <p>1005 N Abbe Rd<br />
