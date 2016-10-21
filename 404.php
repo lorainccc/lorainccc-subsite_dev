de23311a81d5fbf826b22a838f122ffaa3b50a84
Joe Querin <joecue@gmail.com>
2016-10-21 13:30:51 -0400

    Added 404 Template and Updated Footer

---
+++ 404.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found)
+ *
+ * @package lorainccc-subsite
+ */
+
+get_header(); ?>
+<div class="row page-content">
+<div class="medium-4 large-4 columns hide-for-small-only">
+		
+	</div>
+	</div>
+			
+	<div class="small-12 medium-8 large-8 columns">		
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+		<?php //while ( have_posts() ) : the_post(); ?>
+
+				<?php //get_template_part( 'template-parts/content', 'page' ); ?>
+
+			<?php //endwhile; // end of the loop. ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div>	
+	
+</div>
+<?php get_footer(); ?>
+
