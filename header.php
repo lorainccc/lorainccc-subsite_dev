a6c56e029bdf15ad38cb1188c698d9512899fbec
Joe Querin <joecue@gmail.com>
2016-12-22 13:23:08 -0500

    Added Google Analytics Tracking Code

--- header.php
+++ header.php
@@ -14,6 +14,18 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <?php wp_head(); ?>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-7542329-2', 'auto', {'allowLinker': true});
+  ga('require', 'linker');
+  ga('linker:autoLink', ['sites.lorainccc.edu'] );
+  ga('send', 'pageview');
+
+</script>
 </head>
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
@@ -47,7 +59,7 @@
 																// check for plugin using plugin name
 																// Plugin is activated
 																		if ( is_active_sidebar( 'lccc-four-o-four-sidebar' ) ) {
-																		
+
 																		}else{
 																			$instance = array(
 																			'display_results' => '3',
@@ -120,8 +132,8 @@
 													'walker' => new lc_drill_menu_walker(),
 												));
      ?>
-     <li>&nbsp;</li> 
-     <?php      
+     <li>&nbsp;</li>
+     <?php
             wp_nav_menu(array(
 													'container' => false,
 													'menu' => __( 'Drill Menu', 'textdomain' ),
