de38502e289803722b3389d02e1ef75b118af6b3
David Michael Brattoli <dbrattoli@lorainccc.edu>
2017-01-31 16:39:49 -0500

    Updated Event Archive Layout and Styling

--- archive-lccc_events.php
+++ archive-lccc_events.php
@@ -2,161 +2,270 @@
 /**
  * The template for displaying archive pages.
  *
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ * @link https://codex.wordpress.org/Template_Hierarchy
  *
- * @package LCCC Framework
+ * @package MyLCCC_Theme
  */
 
 get_header(); ?>
-<div class="row page-content">
-
-<div class="small-12 medium-12 large-12 columns breadcrumb-container">
-   <?php get_template_part( 'template-parts/content', 'breadcrumb' ); ?> All Events
-</div>
-
-	<div class="small-12 medium-12 large-12 columns nopadding">		
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main mylccc-event-archive" role="main">
-            <div class="small-12 medium-12 large-12 columns">
-				<header class="page-header">
-				<h1 class="page-title"> LCCC Events</h1>
-			</header><!-- .page-header -->
-			<?php
-			$eventlistargs=array(
-			'post_type' => 'page',
-					'post_status' => 'publish',
-				'name' => 'events-list',
-			);
-			$eventsdescrip = new WP_Query($eventlistargs);
-					if ( $eventsdescrip->have_posts() ){
-							while ( $eventsdescrip->have_posts() ) : $eventsdescrip->the_post();
-													
-								the_content('<p>','</p>');
-						?>
-						     <div class="column row event-list-row">
-            <hr>
-            </div> 							
-						<?php
-							endwhile;
-					}else{
-						
-
-					}
-			?>
-      
-		</div>
-             <div class="small-12 medium-12 large-12 columns archive-events-container">
-                <?php 
-		$paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
-                 $eventargs=array(
-                    'post_type' => 'lccc_events',
-		    'posts_per_page' => 5,
-		    'order'=> 'ASC',
-    		    'orderby'=> 'meta_value',
-		    'paged' => $paged,
-    		    'meta_key' => 'event_start_date',
-                );
-					$wp_query = new WP_Query($eventargs);
-					if ( $wp_query->have_posts() ) :
-						while ( $wp_query->have_posts() ) : $wp_query->the_post();
-	$starteventdate = event_meta_box_get_meta('event_start_date');
+	<div class="row main">
+<?php
+$today = getdate();
+				$currentDay = $today['mday'];
+				$month = $today['mon'];
+				$year = $today['year'];
+				$firsteventdate ='';
+    $nexteventdate ='';
+				$todaysevents = '';
+				$temp = strLen($currentDay);            
+				$twoDay = '';
+	   $nextTwoDay = '';
+    if ($temp < 2){
+							$twoDay = '0' . $currentDay;
+				}else{
+							$twoDay = $currentDay;
+				}
+				$twomonth = '';
+    $tempmonth = strLen($month);
+    if ($tempmonth < 2){
+							$twomonth = '0' . $month;
+				}else{
+							$twomonth = $month;
+				}
+			 $nextDay = $currentDay + 1;
+				if ($temp < 2){
+							$nextTwoDay = '0' . $currentDay;
+				}else{
+							$nextTwoDay = $currentDay;
+				}
+		$starteventdate = 
+			event_meta_box_get_meta('event_start_date');
 		$starteventtime = event_meta_box_get_meta('event_start_time');  
 		$endeventdate = event_meta_box_get_meta('event_end_date');
 		$endtime = event_meta_box_get_meta('event_end_time');
-		$starttimevar=strtotime($starteventtime);
-		$starttime=	date("g:i a",$starttimevar);
-		$starteventtimehours = date("G",$starttimevar);
-		$starteventtimeminutes = date("i",$starttimevar);
-		$startdate=strtotime($starteventdate);
-		$eventstartdate=date("Y-m-d",$startdate);
-		$eventstartmonth=date("M",$startdate);
-        $eventstartmonthfull=date("F",$startdate);
-		$eventstartday =date("j",$startdate);
-        $eventstartyear =date("Y",$startdate);
-		$endeventtimevar=strtotime($endtime);
-		$endeventtime = date("h:i a",$endeventtimevar);
-		$endeventtimehours = date("G",$endeventtimevar);
-		$endeventtimeminutes = date("i",$endeventtimevar);
-		$enddate=strtotime($endeventdate);
-		$endeventdate = date("Y-m-d",$enddate);
-		$location = event_meta_box_get_meta('event_meta_box_event_location');  
-        	$cost = event_meta_box_get_meta('event_meta_box_ticket_price_s_');	
-		$key_1_value = get_post_meta( get_the_ID(), 'event_start_date', true );
-			?>
-<div id="post-<?php the_ID(); ?>" class="small-12 medium-12 large-12 columns nopadding">
-	<?php	if ( has_post_thumbnail() ) { ?>
-			<div class="small-12 medium-3 large-3 columns nopadding">
-						<?php the_post_thumbnail();?>
-			</div>
-			<div class="small-12 medium-9 large-9 columns nopadding">
-                        <header class="entry-header">
-        <a href="<?php the_permalink();?>"><?php the_title( '<h1 class="entry-title">', '</h1>' ); ?></a>
-			<div class="taxonomies">
-				<?php echo get_the_term_list( $post->ID, 'event_categories','', ' , ' , ''); ?>
-			</div>
-			<p><?php echo 'Date: '.$eventstartmonthfull.' '.$eventstartday.' , '.$eventstartyear; ?></p>
-        		<p><?php echo 'Time: '.$starttime; ?></p>
-          		<p><?php echo 'Location: '.$location; ?></p>
-										<?php if($cost != ''){ ?>															
-        		<p><?php echo 'Cost: '.$cost; ?></p>
-											<?php } ?>														
-        		<p>&nbsp;</p>
-			</header>
-			<div class="small-12 medium-12 large-12 columns nopadding">
-				<div class="entry-content">
-					<?php the_excerpt();?>
-					<a class="button" href="<?php the_permalink();?>">More Information</a>
-				</div><!-- .entry-content -->
-			</div>
-			</div>
-	<?php }else{ ?>
-	<div class="small-12 medium-12 large-12 columns nopadding mylccc-event">
-   <header class="entry-header">
-        <a href="<?php the_permalink();?>"><?php the_title( '<h1 class="entry-title">', '</h1>' ); ?></a>
-			<div class="taxonomies">
-				<?php echo get_the_term_list( $post->ID, 'event_categories','', ' , ' , ''); ?>
-			</div>
-			<p><?php echo 'Date: '.$eventstartmonthfull.' '.$eventstartday.' , '.$eventstartyear; ?></p>
-        		<p><?php echo 'Time: '.$starttime; ?></p>
-          		<p><?php echo 'Location: '.$location; ?></p>
-        			<?php if($cost != ''){ ?>															
-        		<p><?php echo 'Cost: '.$cost; ?></p>
-											<?php } ?>				
-        		<p>&nbsp;</p>
-			</header>
-			<div class="small-12 medium-12 large-12 columns nopadding">
-				<div class="entry-content">
-					<?php the_excerpt();?>
-					<a class="button" href="<?php the_permalink();?>">More Information</a>
-				</div><!-- .entry-content -->
-			</div>
-			</div>
-	<?php } ?>
-	
-	
-	
-</div>
+		
 
-			 <div class="column row event-list-row">
-            			<hr>
-            		  </div> 
-			 <?php
-			endwhile;
-?> 	
-<div id="pagination" class="clearfix">
-  <div style="float:left;padding-top:2rem;"><?php previous_posts_link( 'Previous Events' ); ?></div>
-  <div style="float:right;padding-top:2rem;"><?php next_posts_link( 'More Events', $wp_query->max_num_pages ); ?></div>
-</div>
-<?php
-		      wp_reset_postdata();
-                    endif;
-                 ?>
-            </div>
+										$starttimevar=strtotime($starteventtime);
+										$starttime=	date("h:i a",$starttimevar);
+										$starteventtimehours = date("G",$starttimevar);
+										$starteventtimeminutes = date("i",$starttimevar);
+		
+          $startdate=strtotime($starteventdate);
+										$eventstartdate=date("Y-m-d",$startdate);
+										$eventstartmonth=date("M",$startdate);
+                                        $eventstartmonthfull=date("F",$startdate);
+										$eventstartday =date("j",$startdate);
+                                        $eventstartyear =date("Y",$startdate);
+										
+										$endeventtimevar=strtotime($endtime);
+										$endeventtime = date("h:i a",$endeventtimevar);
+										$endeventtimehours = date("G",$endeventtimevar);
+										$endeventtimeminutes = date("i",$endeventtimevar);
+		
+										$enddate=strtotime($endeventdate);
+										$endeventdate = date("Y-m-d",$enddate);
+		
+										
+		$duration = '';
+		if($endeventtimehours == 0){
+			$endeventtimehours =24;
+		}
+		$durationhours =	$endeventtimehours - $starteventtimehours;
+		if($durationhours > 0){
+				if($durationhours == 24){
+				$duration .= '1 day';
+				}else{
+				$duration .= $durationhours.'hrs'; 
+				}
+		}
+		$durationminutes = $endeventtimeminutes - $starteventtimeminutes;
+		if($durationminutes > 0){
+			$duration .= $durationminutes.'mins';
+		}
+$location = event_meta_box_get_meta('event_meta_box_event_location');  
+$cost = event_meta_box_get_meta('event_meta_box_ticket_price_s_');
+	$eventsubheading = event_meta_box_get_meta('event_meta_box_sub_heading');
 
+?>
+<div class="small-12 medium-12 large-12 columns contentdiv">
+	<div class="small-12 medium-12 large columns noapadding">
+				<header class="page-header">
+				<h1 class="page-title"> Events</h1>
+				<?php
+					the_archive_description( '<div class="taxonomy-description">', '</div>' );
+		
+                ?>
+			</header><!-- .page-header -->
+	</div>
+	<div class="small-12 medium-8 large-8 columns nopadding">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<div class="small-12 medium-12 large-12 columns">
+			<?php
+					//Defining variables for endpoints
+						$lcccevents = '';
+						$stockerevents = '';
+						$athleticevents = '';
+						
+					//defining the domain variable
+				 //	$domain = 'http://' . $_SERVER['SERVER_NAME'];
+							$domain = 'http://www.lorainccc.edu';
+					//Defining the endpoints
+							$lcccevents = new Endpoint( $domain . '/mylccc/wp-json/wp/v2/lccc_events?filter[posts_per_page]=-1' );
+							$athleticevents = new Endpoint( $domain . '/athletics/wp-json/wp/v2/lccc_events?filter[posts_per_page]=-1' );
+							$stockerevents = new Endpoint( 'http://sites.lorainccc.edu/stocker/wp-json/wp/v2/lccc_events?filter[posts_per_page]=-1' );
+						
+						//Create instance
+							$multi = new MultiBlog( 1 );
+				
+						//Adding endpoints to multi array
+							$multi->add_endpoint ( $lcccevents );
+							$multi->add_endpoint ( $athleticevents );
+							$multi->add_endpoint ( $stockerevents );
+							
+						//Fetch Posts from Endpoints
+							$posts = $multi->get_posts();
+						    $count = count($posts);
+						//Test to see if any events exist 
+							if(empty($posts)){
+									echo 'No Posts Found!';
+							}
+							$icounter = 0;
+							$pagecounter = 1;						
+							$posts_per_page = 10;
+   				            $currentdate = date("Y-m-d");
+							$currentday = date("d");
+							$currentmonth = date("m");
+							$currentmonthname = date("M");
+
+usort( $posts, function ( $a, $b) {
+return strtotime( $a->event_start_date ) - strtotime( $b->event_start_date );
+});
+				$eventcounter = 0;
+				$firstactive = '';
+                
+				foreach ( $posts as $post ){
+							if( $post->event_end_date > $currentdate ){
+										$firstactive = $eventcounter;
+										break;
+							}
+							$eventcounter++;
+				}
+                  
+                //See if there is a vriable page in the URL is set or exists
+                if (isset($_GET['page'])) {
+                    $currentpage = $_GET['page']; 
+                    $advamount = $_GET['page'] * $posts_per_page;
+                    $activepost = $firstactive + $advamount - $posts_per_page;
+                }else{
+                    $currentpage = 1;
+                }
+                
+                //Defining Posts array starting point based on current page
+                 if (isset($_GET['page'])) {
+                     if( $_GET['page'] != 1 ){
+                        $posts = array_slice($posts,$activepost);
+                     }else{
+                        $posts = array_slice($posts,$firstactive);  
+                     }
+                 }else{
+                      $posts = array_slice($posts,$firstactive); 
+                 }		  
+						//$posts will be an array of all posts sorted by post date
+							foreach ( $posts as $post ){
+                                if(	$icounter<$posts_per_page){
+								?>
+								<article class="small-12 medium-12 large-12 columns" id="post-<?php echo $post->id->rendered; ?>" >
+						
+										<header class="entry-header">
+												<a href="<?php echo $post->link; ?>">
+													<?php echo '<h1 class="entry-title">'.$post->title->rendered.'</h1>'; ?>
+											</a>
+										</header><!-- .entry-header -->
+									<?php
+									echo '<div class="small-12 medium-12 large-12 columns nopadding">';
+   									
+									echo '</div>';	
+								?>
+										<div class="small-12 medium-12 large-12 columns entry-content nopadding">
+												<?php
+														//set the variable to see if a featured image exists
+														$featured = $post->featured_media;
+												
+														//Test to see if image exists. If the vaule is equal zero then no image exists
+														if($featured != 0){
+																echo '<div class="small-12 medium-3 large-3 columns nopadding">';
+																	echo '<img src="'.$post->better_featured_image->media_details->sizes->medium->source_url.'" alt="'.$post->better_featured_image->alt_text.'">';
+																echo '</div>';
+															echo '<div class="small-12 medium-9 large-9 columns">';
+												echo '<div class="small-12 medium-12 large-12 columns event-details">';
+															$eventdate = $post->event_start_date;
+															if($eventdate !=''){
+															$newDate = date("F j, Y", strtotime($eventdate));
+															echo '<p>'.'Date: '.$newDate.'</p>';
+															}
+															if($post->event_start_time !=''){
+															echo '<p>'.'Time: '.$post->event_start_time.'</p>';	
+															}
+															$location = $post->event_location;
+															if ( $location != ''){
+																echo '<p>Location: '.$location.'</p>';
+															}
+													echo '</div>';
+													echo '<div class="small-12 medium-12 large-12 columns">';
+														echo '<p>' . $post->excerpt->rendered . '</p>' ;
+														echo '<a class="button" href="'.$post->link.'">More Information</a>';
+													echo '</div>';
+															echo '</div>';
+														}else{
+															echo '<div class="small-12 medium-12 large-12 columns event-details nopadding">';
+															$eventdate = $post->event_start_date;
+																	if($eventdate !=''){
+															$newDate = date("F j, Y", strtotime($eventdate));
+															echo '<p>'.'Date: '.$newDate.'</p>';
+															}
+															if($post->event_start_time !=''){
+															echo '<p>'.'Time: '.$post->event_start_time.'</p>';	
+															}
+															$location = $post->event_location;
+															if ( $location != ''){
+																echo '<p>Location: '.$location.'</p>';
+															}
+													echo '</div>';
+															echo '<div class="small-12 medium-12 large-12 columns nopadding">';
+															echo ' <p>' . $post->excerpt->rendered . '</p>' ; 	
+																echo '<a class="button" href="'.$post->link.'">More Information</a>';
+															echo '</div>';	
+														}
+												?>
+										</div>
+										<div class="column row">
+												<hr>
+										</div>
+									</article>
+									<?php
+											$icounter ++;			
+										}
+								
+							}
+                            //$count = $count - $firstactive;
+							$pagecount = ceil($count/$posts_per_page);
+							
+							echo '<div class="small-12 medium-12 large-12 columns nopadding event-pagination">';
+								for($pagecounter=1;$pagecounter < $pagecount;$pagecounter++){
+                                        echo '<a href="/mylccc/lccc_events/?page='.$pagecounter.'" class="button">'.$pagecounter.'</a>';
+                                }
+							echo '</div>';
+				?>
+			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
-</div>	
-
-</div>
-<?php get_footer(); ?>
-
+	</div>
+	<div class="small-12 medium-4 large-4 columns">	
+<?php
+get_sidebar();
+?>
+	</div>
+</div>		
+		<?php
+get_footer();
--- week.php
+++ week.php
@@ -9,81 +9,21 @@
 
 get_header(); ?>
 <div class="row page-content">
-<div class="small-12 columns nopadding show-for-small-only"><!--Begin Mobile Side Menu -->
- <div class="small-12 medium-12 large-12 columns nopadding">
-  <div class="row show-for-small-only sub-mobile-menu-row hide-for-print" style="background:#000;">
-   <div class="small-2 columns" style="padding-top: 0.5rem;padding-left: 1.625rem;"> <span data-responsive-toggle="sub-responsive-menu" data-hide-for="medium">
-     <button class="menu-icon" type="button" data-toggle></button>
-     </span> </div>
-   <div class="small-10 columns nopadding hide-for-print">
-    <h3 class="sub-mobile-menu-header" style="padding-top: 6px;
-   padding-left: 8px;color:#ffffff ;"><?php echo bloginfo('the-title'); ?></h3></div>
-  </div>
-  <div id="sub-responsive-menu" class="show-for-small-only hide-for-print">
-   <ul class="vertical menu" data-drilldown data-parent-link="true">
 
-    <?php     wp_nav_menu(array(
-                                                    'container' => false,
-                                                    'menu' => __( 'Drill Menu', 'textdomain' ),
-                                                    'menu_class' => 'vertical menu',
-                                        'theme_location' => 'left-nav',
-                                                    'menu_id' => 'sub-mobile-primary-menu',
-                                                        //Recommend setting this to false, but if you need a fallback...
-                                                    'fallback_cb' => 'lc_drill_menu_fallback',
-                                                    'walker' => new lc_drill_menu_walker(),
-                                                ));
-                    ?>
-
-   </ul>
-  </div>
- </div>
-</div><!--End Mobile Side Menu -->
 <div class="small-12 medium-12 large-12 columns breadcrumb-container">
    <?php get_template_part( 'template-parts/content', 'breadcrumb' ); ?>
 </div>
-<div class="medium-4 large-4 columns hide-for-small-only hide-for-print">
-	<div class="small-12 medium-12 large-12 columns sidebar-widget">
-	<?php	if ( has_nav_menu( 'left-nav' ) ) : ?>
-	 <div class="small-12 medium-12 large-12 columns sidebar-menu-header">
-   <h3><?php echo bloginfo('the-title'); ?></h3>
-  </div>
- <div id="secondary" class="medium-12 columns secondary nopadding">
-		<?php if ( has_nav_menu( 'left-nav' ) ) : ?>
-			<nav id="site-navigation" class="main-navigation" role="navigation">
-				<?php
-					// Primary navigation menu.
-					wp_nav_menu( array(
-						'menu_class'     => 'nav-menu',
-						'theme_location' => 'left-nav',
-					) );
-				?>
-			</nav><!-- .main-navigation -->
-				<?php endif; ?>
-		<?php endif; ?>
-  <?php if ( is_active_sidebar( 'lccc-badges-sidebar' ) ) { ?>
-			<div class="small-12 medium-12 large-12 columns hide-for-print">
-			<?php dynamic_sidebar( 'lccc-badges-sidebar' ); ?>
-			</div>
-	<?php } ?>
-	</div>
-	</div>
- 	<!--<div class="small-12 medium-12 large-12 columns">
-				<?php //if ( is_active_sidebar( 'lccc-events-sidebar' ) ) { ?>
-							<?php //dynamic_sidebar( 'lccc-events-sidebar' ); ?>
-				<?php //} ?>
-	</div>-->
-	</div>
-	<div class="small-12 medium-8 large-8 columns">
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+
+	<div class="small-12 medium-12 large-12 columns">
+	<!--<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">-->
 	<?php
-						while ( have_posts() ) : the_post();
-							get_template_part( 'template-parts/content', 'eventscalendar' );
-					endwhile; // End of the loop.
+			//			while ( have_posts() ) : the_post();
+					//		get_template_part( 'template-parts/content', 'eventscalendar' );
+					//endwhile; // End of the loop.
 						?>
-		</main><!-- #main -->
-	</div><!-- #primary -->
-			<div class="small-12 medium-12 large-12 columns">
+	<!--	</main> #main -->
+	<!--</div> #primary -->
 
 	<?php 
 $myvar = get_query_var('d');
@@ -109,6 +49,22 @@ $dateArray = array();
 
 ?>
 <?php 	$lastdate =  $year.'-'.$month.'-'.$day; ?>
+	<div class="small-12 medium-12 large-12 columns nopadding">
+				<?php
+				// What is the first day of the month in question?
+					$firstDayOfMonth = mktime(0,0,0,$month,1,$year);
+	// Retrieve some information about the first day of the
+ // month in question.
+     $dateComponents = getdate($firstDayOfMonth);			
+		// What is the name of the month in question?
+  $monthName = $dateComponents['month'];		
+		
+							echo	'<h1>LCCC Events for the week of '.$monthName.' '.$day.', '.$year.'</h1>';			
+				
+				?>
+				
+				
+				
 			<a href='calendar/?d=<?php echo $lastdate;?>'><-- Back To The Calendar</a><br />	
 <div class="small-up-1 medium-up-3 large-up-3">		
 					<div class="column column-block">
@@ -120,27 +76,32 @@ $dateArray = array();
 	
 						<div class="column column-block"><?php do_action( 'lccc_next_week',$year, $month, $day); ?></div>
 </div>
+</div>
+			<div class="small-12 medium-12 large-12 columns nopadding">
 <?php
-
-//Code for calling functions to generate page
-do_action('lccc_week',$month,$year,$day);
 	
-	// What is the first day of the month in question?
-					$firstDayOfMonth = mktime(0,0,0,$month,1,$year);
 	// How many days does this month contain?
 					$numberDays = date('t',$firstDayOfMonth);
      $daydisplaycounter = '';
-					if($day+7 > $numberDays){
+				
+//Code for calling functions to generate page
+do_action('lccc_week',$month,$year,$day);
+	
+				
+if($day+7 > $numberDays){				
+		//Test to see if the week rolls over into the next month
 							$nxtmonth = $month+1;	
-							$day = 1;
+							$nxtday = 1;
 								if ($nxtmonth == 13){
-										$nxtmonth = 1;
+										$nxtyear = $year;
+									 $nxtmonth = 1;
 										$year ++;
-									}
-							do_action('lccc_add_to_list',$nxtmonth,$year,$day);
-					}
+									}			
+//If the week rolls over into a new month the below functions generates the list of events starting the 1st of the next month
+							do_action('lccc_add_to_list',$nxtmonth,$nxtyear,$nxtday);
+}
 ?>
-</div>
+		</div>
 		
 </div>
 <?php
