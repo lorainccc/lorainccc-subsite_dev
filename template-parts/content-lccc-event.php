0309e1758423576e772cffa4d108e2c2b8d58973
David Michael Brattoli <dbrattoli@lorainccc.edu>
2017-02-02 16:14:47 -0500

    updated single event styling

--- template-parts/content-lccc-event.php
+++ template-parts/content-lccc-event.php
@@ -101,7 +101,7 @@ $cost = event_meta_box_get_meta('event_meta_box_ticket_price_s_');
 					}
 					?>
     </div>
-	<div class="small-12 medium-2 large-2 columns">
+	<div class="small-12 medium-3 large-2 columns">
 	<?php
 			echo '<div class="small-12 medium-12 large-12 columns event-date">';
         echo '<div class="small-12 medium-12 large-12 columns calender">';                
@@ -111,7 +111,7 @@ $cost = event_meta_box_get_meta('event_meta_box_ticket_price_s_');
 			echo '</div>';	
 		?>
  </div>
-	<div class="small-12 medium-10 large-10 columns nopadding">
+	<div class="small-12 medium-9 large-10 columns nopadding">
 	<header class="entry-header">
 <div class="taxonomies">
 	<?php echo get_the_term_list( $post->ID, 'event_categories','', ' , ' , ''); ?>
