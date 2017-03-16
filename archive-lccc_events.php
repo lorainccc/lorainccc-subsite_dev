ef0d99c7b6c72d375098231f4e1879206b73898f
David Michael Brattoli <dbrattoli@lorainccc.edu>
2017-03-16 14:12:57 -0400

    Updated archive and day templates

--- archive-lccc_events.php
+++ archive-lccc_events.php
@@ -111,9 +111,9 @@ $cost = event_meta_box_get_meta('event_meta_box_ticket_price_s_');
 				 //	$domain = 'http://' . $_SERVER['SERVER_NAME'];
 							$domain = 'http://www.lorainccc.edu';
 					//Defining the endpoints
-							$lcccevents = new Endpoint( $domain . '/mylccc/wp-json/wp/v2/lccc_events?filter[posts_per_page]=-1' );
-							$athleticevents = new Endpoint( $domain . '/athletics/wp-json/wp/v2/lccc_events?filter[posts_per_page]=-1' );
-							$stockerevents = new Endpoint( 'http://sites.lorainccc.edu/stocker/wp-json/wp/v2/lccc_events?filter[posts_per_page]=-1' );
+							$lcccevents = new Endpoint( $domain . '/mylccc/wp-json/wp/v2/lccc_events?per_page=100' );
+							$athleticevents = new Endpoint( $domain . '/athletics/wp-json/wp/v2/lccc_events?per_page=100' );
+							$stockerevents = new Endpoint( 'http://sites.lorainccc.edu/stocker/wp-json/wp/v2/lccc_events?per_page=100' );
 						
 						//Create instance
 							$multi = new MultiBlog( 1 );
